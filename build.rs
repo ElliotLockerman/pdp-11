8a4a926380289f1c0c305858178d43b472bbe6a0
Elliot Lockerman <elliot@lockerman.info>
2024-07-05 21:21:44 -0400

    [as] building lalrpop parser out of source.

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 
 fn main() {
-    // lalrpop::process_root().unwrap();
-    lalrpop::Configuration::new().process_file("src/assembler/grammar.lalrpop").unwrap();
+    lalrpop::process_root().unwrap();
 }
 
--- src/assembler.rs
+++ src/assembler.rs
@@ -1,8 +1,12 @@
 
+use lalrpop_util::lalrpop_mod;
+
 pub mod assemble;
 pub mod ir;
-pub mod grammar;
+// pub mod grammar;
 pub mod helpers;
+
+lalrpop_mod!(grammar, "/assembler/grammar.rs");
 
 pub use assemble::assemble;
 
