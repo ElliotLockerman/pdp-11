01f1297e7595c2ac37f4334dfce3fada3d420963
Elliot Lockerman <elliot@lockerman.info>
2024-08-07 10:42:25 -0400

    [interp] interpreter binary that both assembles and emulates, with wrapper script.

---
+++ interpreter/src/main.rs
@@ -0,0 +1,41 @@
+
+use as_lib::assemble_with_symbols;
+use emu_lib::Emulator;
+use emu_lib::io::teleprinter::Teleprinter;
+use emu_lib::io::clock::Clock;
+
+use clap::Parser;
+
+/// PDP-11 Assembly Interpreter
+#[derive(Parser)]
+#[command(about)]
+struct Args {
+    /// Input assembly file
+    input: String,
+
+    /// Symbol at which to start executing
+    #[arg(long, default_value="_start")]
+    start: String,
+}
+
+
+fn main() {
+    env_logger::init();
+
+    let opt = Args::parse();
+    let input = std::fs::read_to_string(opt.input).unwrap();
+    let (bin, symbols) = assemble_with_symbols(input.as_str());
+
+    let teleprinter = Teleprinter::default();
+    let clock = Clock::default();
+    let mut emu = Emulator::new();
+    emu.set_mmio_handler([Teleprinter::TPS, Teleprinter::TPB], teleprinter);
+
+    emu.set_mmio_handler([Clock::LKS], clock);
+
+    emu.load_image(&bin, 0);
+    let Some(start) = symbols.get(&opt.start) else {
+        panic!("Start symbol {} not found", opt.start);
+    };
+    emu.run_at(*start);
+}
