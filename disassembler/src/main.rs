aa5bc0edd9a5e65ed2c294e37c2585cf1e0cb24c
Elliot Lockerman <elliot@lockerman.info>
2024-08-08 16:11:20 -0400

    [dis] disassembler bin removing long stretches of zeros from output.

--- disassembler/src/main.rs
+++ disassembler/src/main.rs
@@ -1,4 +1,8 @@
 
+use disassembler::{Disassembled, disassemble};
+use common::constants::WORD_SIZE;
+
+use std::ops::Range;
 
 use clap::Parser;
 
@@ -9,14 +13,48 @@ struct Args {
     bin: String,
 }
 
+fn remove_long_zeros(disassembly: &mut Vec<Disassembled>) {
+    const THRESH: usize = 8;
+
+    let mut ranges = vec![];
+    let mut range_start = None;
+    for (i, dis) in disassembly.iter().enumerate() {
+        if dis.repr.len() == 1 && dis.repr[0] == 0 {
+            if range_start.is_none() {
+                range_start = Some(i);
+            }
+        } else if let Some(start) = range_start {
+            ranges.push(Range{start, end: i});
+            range_start = None;
+        }       
+    }
+
+    for range in ranges.iter().rev() {
+        if range.len() > THRESH {
+            // Leave the first and last, an ellipses will be added between.
+            disassembly.drain(range.start + 1..range.end - 1);
+        }
+    }
+}
+
 
 fn main() {
     env_logger::init();
 
     let args = Args::parse();
     let bin = std::fs::read(args.bin).unwrap();
-    let disassembly = disassembler::disassemble(&bin);
+    let mut disassembly = disassemble(&bin);
+
+    remove_long_zeros(&mut disassembly);
+
+    let mut prev: Option<Disassembled> = None;
     for dis in disassembly {
+        if let Some(p) = &prev {
+            if (p.addr as usize) + p.repr.len() * (WORD_SIZE as usize) != (dis.addr as usize) {
+                println!("...");
+            }
+        }
         println!("{}", dis);
+        prev = Some(dis);
     }
 }
